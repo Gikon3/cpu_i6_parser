1adcdf004dc4b43edf6a96cd57acd97b04ddec66
aliguk <gukov0aleksey@gmail.com>
2020-03-16 10:21:32 +0300

    change format data_in

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
 import re
 
-from base_parser import BaseParser
 from alu_parser import AluParser
 from uart_parser import UartParser
 from memory_parser import MemoryParser
@@ -9,33 +8,27 @@ from memory_map import MemoryMap
 
 remove_death_time = True
 
-# files_in = ["logs_input/session_216_short.log",
-#             "logs_input/session_216.log",
-#             "logs_input/session_217.log",
-#             "logs_input/session_218.log",
-#             "logs_input/session_265.log",
-#             "logs_input/session_266.log",
-#             "logs_input/session_267.log",
-#             "logs_input/session_82_cpu_i6_1_Xe_short.log",
-#             "logs_input/session_82_cpu_i6_1_Xe.log",
-#             "logs_input/session_83_cpu_i6_2_Xe_short.log",
-#             "logs_input/session_83_cpu_i6_2_Xe.log",
-#             "logs_input/session_84_cpu_i6_3_Xe_short.log",
-#             "logs_input/session_84_cpu_i6_3_Xe.log"]
-
-files_in = ["logs_input/session_82_cpu_i6_1_Xe_short.log",
-            "logs_input/session_82_cpu_i6_1_Xe.log",
-            "logs_input/session_83_cpu_i6_2_Xe_short.log",
-            "logs_input/session_83_cpu_i6_2_Xe.log",
-            "logs_input/session_84_cpu_i6_3_Xe_short.log",
-            "logs_input/session_84_cpu_i6_3_Xe.log"]
-
-files_excel = ["cosrad_excel/82_2020-3-3_23i16i27.xls",
-               "cosrad_excel/82_2020-3-3_23i16i27.xls",
-               "cosrad_excel/83_2020-3-4_0i55i26.xls",
-               "cosrad_excel/83_2020-3-4_0i55i26.xls",
-               "cosrad_excel/84_2020-3-4_1i33i48.xls",
-               "cosrad_excel/84_2020-3-4_1i33i48.xls", ]
+data_in = [["logs_input/session_216_short.log", ""],
+           ["logs_input/session_216.log", ""],
+           ["logs_input/session_217.log", ""],
+           ["logs_input/session_218.log", ""],
+           ["logs_input/session_265.log", ""],
+           ["logs_input/session_266.log", ""],
+           ["logs_input/session_267.log", ""],
+           ["logs_input/session_82_cpu_i6_1_Xe_short.log", "cosrad_excel/82_2020-3-3_23i16i27.xls"],
+           ["logs_input/session_82_cpu_i6_1_Xe.log", "cosrad_excel/82_2020-3-3_23i16i27.xls"],
+           ["logs_input/session_83_cpu_i6_2_Xe_short.log", "cosrad_excel/83_2020-3-4_0i55i26.xls"],
+           ["logs_input/session_83_cpu_i6_2_Xe.log", "cosrad_excel/83_2020-3-4_0i55i26.xls"],
+           ["logs_input/session_84_cpu_i6_3_Xe_short.log", "cosrad_excel/84_2020-3-4_1i33i48.xls"],
+           ["logs_input/session_84_cpu_i6_3_Xe.log", "cosrad_excel/84_2020-3-4_1i33i48.xls"],
+           ["logs_input/session_245_cpu_i6_1_Ar.log", ""],
+           ["logs_input/session_246_cpu_i6_1_Ar_dop.log", ""],
+           ["logs_input/session_247_cpu_i6_2_Ar.log", ""],
+           ["logs_input/session_248_cpu_i6_2_Ar_dop.log", ""],
+           ["logs_input/session_249_cpu_i6_3_Ar.log", ""],
+           ["logs_input/session_250_cpu_i6_3_Ar_dop.log", ""],
+           ["logs_input/session_281_cpu_i6_1_Kr.log", ""],
+           ["logs_input/session_282_cpu_i6_2_Kr.log", ""]]
 
 alu_dir_out = "errors_alu"
 uart_dir_out = "errors_uart"
@@ -52,31 +45,33 @@ def create_dir(path_dir):
         os.mkdir(path_dir)
 
 
-base = BaseParser()
-alu = AluParser(remove_death_time)
-uart = UartParser(remove_death_time)
-memory = MemoryParser(remove_death_time)
-map = MemoryMap()
-
 with open(number_errors_filename, 'w') as f:
     pass
 
-for file, cosrad_table in zip(files_in, files_excel):
+for file, cosrad_table in data_in:
+    alu = AluParser(remove_death_time if cosrad_table else False)
+    uart = UartParser(remove_death_time if cosrad_table else False)
+    memory = MemoryParser(remove_death_time if cosrad_table else False)
+    memory_map = MemoryMap()
+
     print("{0:s} is being processed".format(file))
     with open(file, 'r') as f:
         lines = f.readlines()
 
     alu_errors = alu.find_error(lines, cosrad_table)
+    del alu
     create_dir(alu_dir_out)
     with open("{0:s}/alu_{1:s}".format(alu_dir_out, file.split('/')[1]), 'w') as f:
         json.dump(alu_errors[1], f, indent=2)
 
     uart_errors = uart.find_error(lines, cosrad_table)
+    del uart
     create_dir(uart_dir_out)
     with open("{0:s}/uart_{1:s}".format(uart_dir_out, file.split('/')[1]), 'w') as f:
         json.dump(uart_errors[1], f, indent=2)
 
     memory_errors = memory.find_error(lines, cosrad_table)
+    del memory
     create_dir(memory_dir_out)
     with open("{0:s}/memory_{1:s}".format(memory_dir_out, file.split('/')[1]), 'w') as f:
         json.dump(memory_errors[1], f, indent=2)
@@ -89,7 +84,8 @@ for file, cosrad_table in zip(files_in, files_excel):
         f.writelines(numbers_errors)
 
     map_data = [[[a[2], a[3]] for a in pack] for pack in memory_errors[1]]
-    map_coords = map.calculate(map_data)
+    map_coords = memory_map.calculate(map_data)
+    del memory_map
 
     create_dir(memory_map_dir_out)
     with open("{0:s}/map_{1:s}".format(memory_map_dir_out, file.split('/')[1]), 'w') as f:
